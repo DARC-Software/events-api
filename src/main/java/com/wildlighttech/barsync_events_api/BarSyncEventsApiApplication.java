94ba73fd540cf51ecd8ea1c60d4e2ef9e40847a3
BuildTools <aidencooper23@gmail.com>
2025-02-03 22:11:51 -0500

    Initial commit.

---
+++ src/main/java/com/wildlighttech/barsync_events_api/BarSyncEventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.wildlighttech.barsync_events_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarSyncEventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarSyncEventsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildlighttech/barsync_events_api/BarSyncEventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildlighttech.barsync_events_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarSyncEventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
