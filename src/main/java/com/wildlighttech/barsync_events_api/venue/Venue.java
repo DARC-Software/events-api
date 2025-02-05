5a41167b7ee5f98c37aa0185ca63fb71a3641e7e
AidenCooper <aidencooper23@gmail.com>
2025-02-05 00:47:23 -0500

    Require an id for every venue.

--- src/main/java/com/wildlighttech/barsync_events_api/venue/Venue.java
+++ src/main/java/com/wildlighttech/barsync_events_api/venue/Venue.java
@@ -1,4 +1,4 @@
 package com.wildlighttech.barsync_events_api.venue;
 
-public record Venue(String name, String address, String phoneNumber) {
+public record Venue(Long id, String name, String address, String phoneNumber) {
 }
